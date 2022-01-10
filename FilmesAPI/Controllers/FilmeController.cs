b3181ad0f5d03d283b7bd8acc511b03e0a754b13
Douglas Shibata <douglas.shibata11@gmail.com>
2022-01-10 14:38:08 -0300

    cadastrando e listando os filmes via banco de dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,27 +12,31 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult adicionarFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+            _context.filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(GetFilmeById), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IActionResult getFilmes()
+        public IEnumerable<Filme> getFilmes()
         {
-            return Ok(filmes);
+            return _context.filmes;
         }
 
         [HttpGet("{id}")]
         public IActionResult GetFilmeById(int id)
         {
-            Filme filme = filmes.FirstOrDefault(filmes => filmes.Id == id);
+            Filme filme = _context.filmes.FirstOrDefault(filmes => filmes.Id == id);
             if(filme != null)
             {
                 return Ok(filme);
