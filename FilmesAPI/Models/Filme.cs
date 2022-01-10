a1202a0fd75e17b5b0ce722d242c3b98d785c7e1
Douglas Shibata <douglas.shibata11@gmail.com>
2022-01-10 10:48:22 -0300

    Realizando post com validações nos filmes

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage ="O campo titulo é obrigatório")]
         public string Titulo { get; set; }
 
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
+        
+        [StringLength(30,ErrorMessage = "O genero não pode passar de 30 caracteres")]
         public string Genero { get; set; }
+
+        [Range(1,600,ErrorMessage ="A duração deve ter no minimo 1 e máximo 600 minutos")]
         public int Duracao { get; set; }
     }
 }
