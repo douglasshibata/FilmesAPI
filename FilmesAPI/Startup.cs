63cf8d1ad896a29bac5cd91f8bf5a16b21b5dfdc
Douglas Shibata <douglas.shibata11@gmail.com>
2022-01-10 12:58:55 -0300

    adicionando conexão com o banco de dados

--- FilmesAPI/Startup.cs
+++ FilmesAPI/Startup.cs
@@ -1,3 +1,5 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace FilmesAPI
 {
@@ -26,7 +29,7 @@ namespace FilmesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(Configuration.GetConnectionString("FilmeConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
